44450841ca61954efc6ad2655ad2f01f2d81a57b
Wenwen1477 <163106315+Wenwen1477@users.noreply.github.com>
2025-03-10 13:55:04 +0700

    Final

--- Assets/Gravity.cs
+++ Assets/Gravity.cs
@@ -1,29 +1,55 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-    Rigidbody rb;
+    public static List<Gravity> objectsWithGravity = new List<Gravity>();
+    public Rigidbody rb;
+    public bool isOrbiting = false;
+    public float initialOrbitSpeed = 5f;
+
     const float G = 0.00674f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        objectsWithGravity.Add(this);
+
+        if (isOrbiting && objectsWithGravity.Count > 1)
+        {
+            Gravity centralBody = objectsWithGravity[0];
+            Vector3 orbitDirection = Vector3.Cross(Vector3.up, (rb.position - centralBody.rb.position).normalized);
+            rb.velocity = orbitDirection * initialOrbitSpeed;
+        }
     }
 
     private void FixedUpdate()
     {
-        //All Attract
+        foreach (Gravity obj in objectsWithGravity)
+        {
+            if (obj != this)
+            {
+                Attract(obj);
+            }
+        }
     }
 
-    void Attract(GravityA other)
+    void Attract(Gravity other)
     {
-        Rigidbody rbOther = GetComponent<Rigidbody>();
-        Vector3 direction = rb.position - other.rb.position;
+        Rigidbody rbOther = other.rb;
+        Vector3 direction = rb.position - rbOther.position;
         float distance = direction.magnitude;
-        if (distance == 0) { return; }
 
-        float forceMagnitude = G * (rb.mass * other.rbOther.mass) / Mathf.Pow(distance, 2);
+        if (distance == 0) return;
+
+        float forceMagnitude = G * (rb.mass * rbOther.mass) / Mathf.Pow(distance, 2);
         Vector3 force = forceMagnitude * direction.normalized;
+
+        rbOther.AddForce(force);
     }
-}
+
+    private void OnDestroy()
+    {
+        objectsWithGravity.Remove(this);
+    }
 }
